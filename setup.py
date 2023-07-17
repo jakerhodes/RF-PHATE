944c237096a86925f5a351faf6ffa74578018c60
Jake Rhodes <jakerhodes8@gmail.com>
2023-07-17 13:58:20 -0600

    initialize setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+
+def parse_requirements(filename):
+    with open(filename, 'r') as file:
+        lines = file.readlines()
+        return [line.strip() for line in lines]
+    
+    
+setup(
+    name = 'RF-PHATE',
+    version = '0.1.0',
+    description = 'The package is used to generate RF-PHATE manifold embeddings based on RF-GAP or other RF proximities',
+    author = 'Jake Rhodes',
+    author_email = 'jakerhodes8@gmail.com',
+    packages = find_packages(),
+    install_requires = parse_requirements('requirements.txt')
+)
