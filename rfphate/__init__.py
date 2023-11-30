048b271bdaf43c3eb9529efdb19728aaad20dca2
Jake Rhodes <jakerhodes8@gmail.com>
2023-11-30 09:43:07 -0700

    add metrics to __init__.py

--- rfphate/__init__.py
+++ rfphate/__init__.py
@@ -2,3 +2,7 @@ from .rfphate import RFPHATE
 import rfphate.rfgap
 from rfphate.dataprep import dataprep
 from rfphate.load_data import load_data
+
+from rfphate.metrics.GSP import global_structure_preservation
+from rfphate.metrics.LGS import low_dimensional_group_separation
+from rfphate.metrics.LSP import local_structure_preservation
