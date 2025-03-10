5a3f5527abf762efcf417e7c664b527e64d2d320
Jake Rhodes <jakerhodes8@gmail.com>
2025-03-10 15:53:10 -0600

    add classifiers and update version

--- setup.py
+++ setup.py
@@ -9,13 +9,26 @@ with open('README.rst', 'r', encoding='utf-8') as f:
     long_description = f.read()
     
 setup(
-    name = 'rfphate',
-    version = '0.1.1',
-    description = 'The package is used to generate RF-PHATE manifold embeddings based on RF-GAP or other RF proximities',
-    author = 'Jake Rhodes',
-    author_email = 'jakerhodes8@gmail.com',
-    packages = find_packages(),
-    install_requires = parse_requirements('requirements.txt'),
-    package_data = {'rfphate': ['datasets/*.csv']},
-    long_description = long_description
+    name='rfphate',
+    version='0.1.2',
+    description='The package is used to generate RF-PHATE manifold embeddings based on RF-GAP or other RF proximities',
+    author='Jake Rhodes',
+    author_email='jakerhodes8@gmail.com',
+    url='https://github.com/jakerhodes8/rfphate',
+    packages=find_packages(),
+    install_requires=parse_requirements('requirements.txt'),
+    package_data={'rfphate': ['datasets/*.csv']},
+    long_description=long_description,
+    long_description_content_type='text/x-rst',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
 )
